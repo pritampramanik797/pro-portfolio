4233899f6a024f196b9b00dafd42b5e80fb87d52
Pritam Pramanik <pritam.pramanik.ece26@heritageit.edu.in>
2025-03-21 12:58:45 +0530

    changed about section

--- src/components/sections/AboutSection.jsx
+++ src/components/sections/AboutSection.jsx
@@ -9,9 +9,7 @@ function AboutSection() {
       Hey, I’m Pritam—a video editor with 3+ years of experience, helping content creators and businesses stand out with high-quality, engaging videos.
       </p>
       <p className="text-sm leading-6 text-black text-opacity-70">
-        With over 5 years of experience in creating visually stunning and
-        user-centric websites, I've honed my skills in both front-end and
-        back-end development.
+      With 1M+ views across my edits, I specialize in turning raw footage into impactful stories. My clients trust me for delivering consistent, on-brand, and professional edits that drive results.
       </p>
     </section>
   );
